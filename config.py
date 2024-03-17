14171d37e8cfb7b8997ed9e4349259bd9f9c5303
MaffazMad <john@madnfts.io>
2024-03-17 22:05:23 +0100

    [UPD] surpress python console

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 URL = "https://api.kucoin.com"
-TOKENS = ("BTC", "ETH", "MOVR", "DOT", "MAP", "ROUP")
-DELAY = 30  # Time delay between updates
+TOKENS = (
+    "BTC", 
+    "ETH", 
+    "SUI", 
+    "SEI", 
+    "MOVR", 
+    "MAP", 
+    "ROUP"
+    )
+DELAY = 5  # Time delay between updates
 AVERAGE_CHAR_WIDTH = 8.5
 MIN_WIDTH = 900
